10f5e9bc157876de93a1d5e9d3f5ace02be988c7
Dilleswar Nettimi <dilleswar1603.n@gmail.com>
2022-03-21 13:33:28 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryNewsComponent } from './components/category-news/category-news.component';
+import { TopNewsComponent } from './components/top-news/top-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : "", component: TopNewsComponent},
+  { path : "category/:categoryType", component: CategoryNewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
